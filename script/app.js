3aab28e84bd117e69ea3074b841c3f5938655990
Octavie Ploye <octavieploye@MacBook-Air-de-Octavie.local>
2021-10-15 00:55:37 +0200

    final version

--- script/app.js
+++ script/app.js
@@ -7,50 +7,53 @@ function init() {
     const scoretDisplay = document.querySelector(".score");
 
     const invadersMove = document.querySelectorAll(".invader");
-    //const aliensIndicesRow1 = [0, 2, 4, 6, 8, 11, 13, 15, 17, 22, 24, 26, 28]
-
+    const invadersIndex = [0, 2, 4, 6, 8, 11, 13, 15, 17, 22, 24, 26, 28]
+    let invadersIndexBlock = 0
+    let invadersIndexBlock2 = 2
+    let invadersIndexBlock4 = 4
+    let invadersIndexBlock6 = 6
+    let invadersIndexBlock8 = 8
+    let invadersIndexBlock11 = 11
+    let invadersIndexBlock13 = 13
+    let invadersIndexBlock15 = 15
+    let invadersIndexBlock17 = 17
+    let invadersIndexBlock22 = 22
+    let invadersIndexBlock28 = 28
+    let invadersIndexBlock24 = 24
+    let invadersIndexBlock26 = 26
 
     let direction = 1;
-    let invadersIndexBlock = 0
-    let invadersRemoved = [];
     let invadersStop;
-    let lives = 3;
     let score = 0;
 
     function start() {
         invadersStop = setInterval(moveInvaders, 1000)
-        invadersStop = setInterval(moveInvaders2, 1000)
+        invadersStop2 = setInterval(moveInvaders2, 1000)
+        invadersStop4 = setInterval(moveInvaders4, 1000)
+        invadersStop6 = setInterval(moveInvaders6, 1000)
+        invadersStop8 = setInterval(moveInvaders8, 1000)
+        invadersStop11 = setInterval(moveInvaders11, 1000)
+        invadersStop13 = setInterval(moveInvaders13, 1000)
+        invadersStop15 = setInterval(moveInvaders15, 1000)
+        invadersStop17 = setInterval(moveInvaders17, 1000)
+        invadersStop22 = setInterval(moveInvaders22, 1000)
+        invadersStop24 = setInterval(moveInvaders24, 1000)
+        invadersStop26 = setInterval(moveInvaders26, 1000)
+        invadersStop28 = setInterval(moveInvaders28, 1000)
     }
+
 
     function reset() {
 
-
         window.location.reload()
-        clearInterval(invadersStop)
-        console.log(reset)
 
     }
 
-
-    //todo remove/add invaders
-
-    function removeInvader(invadersMove) {
-        cells[invadersIndexBlock].classList.remove("invader");
-    }
-
-    removeInvader();
-    console.log(removeInvader);
-
-    function addInvader() {
-        cells[invadersIndexBlock].classList.add("invader");
-        console.log(addInvader)
-    }
-
-    addInvader();
 
     let currentSpaceshipIndex = 94;
 
     // todo move spaceship left/right
+
     cells[currentSpaceshipIndex].classList.add("spaceShip");
 
     function moveSpaceShip(e) {
@@ -75,413 +78,480 @@ function init() {
 
     //todo Move invaders
 
+    //todo remove/add invaders
 
+    function removeInvader() {
+        cells[invadersIndexBlock].classList.remove("invader");
+
+
+    }
+
+    //removeInvader();
+
+    function addInvader() {
+        cells[invadersIndexBlock].classList.add("invader");
+
+    }
+
+    //addInvader();
 
     //todo Robin version
 
+
+    //addInvader();
+
+
     function moveInvaders() {
-        // const leftEdge = invadersMove[0] % 10 === 0;
-        // const rightEdge = invadersMove[invadersMove.length] % 9 === -1;
-        removeInvader();
+        removeInvader()
+
 
         invadersIndexBlock += direction
 
+        addInvader();
 
-        if (invadersIndexBlock === 10) {
 
-            direction = -direction;
-            invadersIndexBlock += 9
-
-        }
         if (invadersIndexBlock > 89) {
-            scoretDisplay.innerHTML = "GAME OVER";
-            clearInterval(invadersStop);
-            //removeInvader(invadersIndexBlock)
+            scoretDisplay.innerHTML = "GAME OVER"
+            clearInterval(invadersStop)
         }
 
-        addInvader(invadersIndexBlock);
-
-
-        if (invadersRemoved.length === invadersMove.length) {
-            scoreDisplay.innerHTML = "YOU WIN";
-            clearInterval(invadersStop);
-        }
     }
 
-    //invadersStop = setInterval(moveInvaders, 1000);
-
     //todo invader 2
-    let invadersIndexBlock2 = 2
+
 
     //todo remove/add invaders
 
     function removeInvader2(invadersMove) {
         cells[invadersIndexBlock2].classList.remove("invader");
     }
-
-    removeInvader2();
-
+    //removeInvader2()
 
     function addInvader2() {
         cells[invadersIndexBlock2].classList.add("invader");
 
     }
 
-    addInvader2();
+    //addInvader2();
 
     //todo Move invaders 2
 
     function moveInvaders2() {
-        // const leftEdge = invadersMove[0] % 10 === 0;
-        // const rightEdge = invadersMove[invadersMove.length - 1] % 9 === -1;
+
+
         removeInvader2();
 
         invadersIndexBlock2 += direction
 
+        addInvader2();
 
-        if (invadersIndexBlock2 === 10) {
-
-            direction = -direction;
-            invadersIndexBlock2 += 9
-        }
         if (invadersIndexBlock2 > 89) {
-            scoretDisplay.innerHTML = "GAME OVER";
-            clearInterval(invadersStop);
-            //removeInvader(invadersIndexBlock)
+            scoretDisplay.innerHTML = "GAME OVER"
+            clearInterval(invadersStop2)
         }
 
-        addInvader2(invadersIndexBlock2);
-
-
-        if (invadersRemoved.length === invadersMove.length) {
-            scoreDisplay.innerHTML = "YOU WIN";
-            clearInterval(invadersStop);
-        }
     }
 
-    //invadersStop = setInterval(moveInvaders2, 1000);
+    //todo invader 4
 
-
-    //     //todo invader 4
-
-    let invadersIndexBlock4 = 4
-
-    // //todo remove/add invaders
+    //todo remove/add invaders
 
     function removeInvader4(invadersMove) {
         cells[invadersIndexBlock4].classList.remove("invader");
     }
 
-    removeInvader4();
-    console.log(removeInvader4);
+
 
     function addInvader4() {
         cells[invadersIndexBlock4].classList.add("invader");
-        console.log(addInvader4)
+
     }
 
-    addInvader4();
 
-    // //todo Move invaders4
+
+    //todo Move invaders4
 
     function moveInvaders4() {
-        // const leftEdge = invadersMove[0] % 10 === 0;
-        // const rightEdge = invadersMove[invadersMove.length - 1] % 9 === -1;
+
         removeInvader4();
 
         invadersIndexBlock4 += direction
 
+        addInvader4();
 
-        if (invadersIndexBlock4 === 10) {
-
-            direction = -direction;
-            invadersIndexBlock4 += 9
-        }
         if (invadersIndexBlock4 > 89) {
-            scoretDisplay.innerHTML = "GAME OVER";
-            clearInterval(invadersStop);
-            //removeInvader(invadersIndexBlock)
-        }
-
-        addInvader4(invadersIndexBlock4);
-
-
-        if (invadersRemoved.length === invadersMove.length) {
-            scoreDisplay.innerHTML = "YOU WIN";
-            clearInterval(invadersStop);
+            scoretDisplay.innerHTML = "GAME OVER"
+            clearInterval(invadersStop4)
         }
     }
 
 
 
-    // invadersStop = setInterval(moveInvaders4, 1000)
+    //todo invader 6
 
-    // //todo invader 6
+    //todo remove/add invaders
 
-    // let invadersIndexBlock6 = 6
+    function removeInvader6(invadersMove) {
+        cells[invadersIndexBlock6].classList.remove("invader");
+    }
 
-    // //todo remove/add invaders
-
-    // function removeInvader6(invadersMove) {
-    //     cells[invadersIndexBlock6].classList.remove("invader");
-    // }
-
-    // removeInvader6();
-    // console.log(removeInvader6);
-
-    // function addInvader6() {
-    //     cells[invadersIndexBlock6].classList.add("invader");
-    //     console.log(addInvader6)
-    // }
-
-    // addInvader6();
-
-    // //todo Move invaders 6
+    //removeInvader6();
 
 
+    function addInvader6() {
+        cells[invadersIndexBlock6].classList.add("invader");
 
-    // invadersStop = setInterval(moveInvaders6, 1000)
+    }
 
-    // //todo invader 8
+    //addInvader6();
 
-    // let invadersIndexBlock8 = 8
+    //todo Move invaders 6
 
-    // //todo remove/add invaders
+    function moveInvaders6() {
 
-    // function removeInvader8(invadersMove) {
-    //     cells[invadersIndexBlock8].classList.remove("invader");
-    // }
+        removeInvader6();
 
-    // removeInvader8();
-    // console.log(removeInvader8);
+        invadersIndexBlock6 += direction
 
-    // function addInvader8() {
-    //     cells[invadersIndexBlock8].classList.add("invader");
-    //     console.log(addInvader8)
-    // }
+        addInvader6();
+        if (invadersIndexBlock6 > 89) {
+            scoretDisplay.innerHTML = "GAME OVER"
+            clearInterval(invadersStop6)
+        }
 
-    // addInvader8();
+    }
+
+
+    //todo invader 8
+
+
+    //todo remove/add invaders
+
+    function removeInvader8(invadersMove) {
+        cells[invadersIndexBlock8].classList.remove("invader");
+    }
+
+    //removeInvader8();
+
+
+    function addInvader8() {
+        cells[invadersIndexBlock8].classList.add("invader");
+
+    }
+
+    //addInvader8();
 
     // //todo Move invaders 8
 
+    function moveInvaders8() {
 
+        removeInvader8();
 
+        invadersIndexBlock8 += direction
 
-    // invadersStop = setInterval(moveInvaders8, 1000)
+        addInvader8();
+
+        if (invadersIndexBlock8 > 89) {
+            scoretDisplay.innerHTML = "GAME OVER"
+            clearInterval(invadersStop8)
+        }
+
+    }
 
     // //todo invader 11
 
-    // let invadersIndexBlock11 = 11
-
     // //todo remove/add invaders
 
-    // function removeInvader11(invadersMove) {
-    //     cells[invadersIndexBlock11].classList.remove("invader");
-    // }
+    function removeInvader11(invadersMove) {
+        cells[invadersIndexBlock11].classList.remove("invader");
+    }
 
-    // removeInvader11();
-    // console.log(removeInvader11);
+    //removeInvader11();
 
-    // function addInvader11() {
-    //     cells[invadersIndexBlock11].classList.add("invader");
-    //     console.log(addInvader11)
-    // }
 
-    // addInvader11();
+    function addInvader11() {
+        cells[invadersIndexBlock11].classList.add("invader");
+
+    }
+
+    //addInvader11();
 
     // //todo Move invaders 11
 
+    function moveInvaders11() {
 
+        removeInvader11();
 
-    // invadersStop = setInterval(moveInvaders11, 3000)
+        invadersIndexBlock11 += direction
+
+        addInvader11();
+
+        if (invadersIndexBlock11 > 89) {
+            scoretDisplay.innerHTML = "GAME OVER"
+            clearInterval(invadersStop11)
+        }
+
+    }
 
     // //todo invader 13
 
-    // let invadersIndexBlock13 = 13
-
     // //todo remove/add invaders
 
-    // function removeInvader13(invadersMove) {
-    //     cells[invadersIndexBlock13].classList.remove("invader");
-    // }
+    function removeInvader13(invadersMove) {
+        cells[invadersIndexBlock13].classList.remove("invader");
+    }
 
-    // removeInvader13();
+    //removeInvader13();
 
 
-    // function addInvader13() {
-    //     cells[invadersIndexBlock13].classList.add("invader");
+    function addInvader13() {
+        cells[invadersIndexBlock13].classList.add("invader");
 
-    // }
+    }
 
-    // addInvader13();
+    //addInvader13();
 
     // //todo Move invaders 13
 
+    function moveInvaders13() {
 
+        removeInvader13();
 
-    // invadersStop = setInterval(moveInvaders13, 4000)
+        invadersIndexBlock13 += direction
 
-    // //todo invader 15
+        addInvader13();
 
-    // let invadersIndexBlock15 = 15
+        if (invadersIndexBlock13 > 89) {
+            scoretDisplay.innerHTML = "GAME OVER"
+            clearInterval(invadersStop13)
+        }
 
-    // //todo remove/add invaders
+    }
 
-    // function removeInvader15(invadersMove) {
-    //     cells[invadersIndexBlock15].classList.remove("invader");
-    // }
 
-    // removeInvader15();
+    //todo invader 15
 
 
-    // function addInvader15() {
-    //     cells[invadersIndexBlock15].classList.add("invader");
 
-    // }
+    //todo remove/add invaders
 
-    // addInvader15();
+    function removeInvader15(invadersMove) {
+        cells[invadersIndexBlock15].classList.remove("invader");
+    }
 
-    // //todo Move invaders 15
+    //removeInvader15();
 
 
+    function addInvader15() {
+        cells[invadersIndexBlock15].classList.add("invader");
 
-    // invadersStop = setInterval(moveInvaders15, 5000)
+    }
 
-    // //todo invader 17
+    //addInvader15();
 
-    // let invadersIndexBlock17 = 17
-    //     //todo remove/add invaders
+    //todo Move invaders 15
 
-    // function removeInvader17(invadersMove) {
-    //     cells[invadersIndexBlock17].classList.remove("invader");
-    // }
+    function moveInvaders15() {
 
-    // removeInvader17();
+        removeInvader15();
 
+        invadersIndexBlock15 += direction
 
-    // function addInvader17() {
-    //     cells[invadersIndexBlock17].classList.add("invader");
 
-    // }
+        addInvader15();
 
-    // addInvader17();
+        if (invadersIndexBlock15 > 89) {
+            scoretDisplay.innerHTML = "GAME OVER"
+            clearInterval(invadersStop15)
+        }
 
-    // //todo Move invaders 17
 
+    }
 
+    //todo invader 17
 
-    // invadersStop = setInterval(moveInvaders17, 900)
 
-    // //todo invader 22
+    //todo remove/add invaders
 
-    // let invadersIndexBlock22 = 22
-    //     //todo remove/add invaders
+    function removeInvader17(invadersMove) {
+        cells[invadersIndexBlock17].classList.remove("invader");
+    }
 
-    // function removeInvader22(invadersMove) {
-    //     cells[invadersIndexBlock22].classList.remove("invader");
-    // }
+    //removeInvader17();
 
-    // removeInvader22();
 
+    function addInvader17() {
+        cells[invadersIndexBlock17].classList.add("invader");
 
-    // function addInvader22() {
-    //     cells[invadersIndexBlock22].classList.add("invader");
+    }
 
-    // }
+    //addInvader17();
 
-    // addInvader22();
+    //todo Move invaders 17
 
-    // //todo Move invaders
+    function moveInvaders17() {
 
+        removeInvader17();
 
+        invadersIndexBlock17 += direction
 
-    // invadersStop = setInterval(moveInvaders22, 300)
+        addInvader17();
 
-    // //todo invader 24
+        if (invadersIndexBlock17 > 89) {
+            scoretDisplay.innerHTML = "GAME OVER"
+            clearInterval(invadersStop17)
+        }
 
-    // let invadersIndexBlock24 = 24
-    //     //todo remove/add invaders
+    }
 
-    // function removeInvader24(invadersMove) {
-    //     cells[invadersIndexBlock24].classList.remove("invader");
-    // }
+    //todo invader 22
 
-    // removeInvader24();
 
+    //todo remove/add invaders
 
-    // function addInvader24() {
-    //     cells[invadersIndexBlock24].classList.add("invader");
+    function removeInvader22(invadersMove) {
+        cells[invadersIndexBlock22].classList.remove("invader");
+    }
 
-    // }
+    //removeInvader22();
 
-    // addInvader24();
 
-    // //todo Move invaders
+    function addInvader22() {
+        cells[invadersIndexBlock22].classList.add("invader");
 
+    }
 
+    //addInvader22();
 
+    // //todo Move invaders 22
 
-    // invadersStop = setInterval(moveInvaders24, 300)
+    function moveInvaders22() {
 
-    // //todo invader 26
+        removeInvader22();
 
+        invadersIndexBlock22 += direction
 
-    // let invadersIndexBlock26 = 26
-    //     //todo remove/add invaders
+        addInvader22();
 
-    // function removeInvader26(invadersMove) {
-    //     cells[invadersIndexBlock26].classList.remove("invader");
-    // }
+        if (invadersIndexBlock22 > 89) {
+            scoretDisplay.innerHTML = "GAME OVER"
+            clearInterval(invadersStop22)
+        }
 
-    // removeInvader26();
+    }
 
 
-    // function addInvader26() {
-    //     cells[invadersIndexBlock26].classList.add("invader");
 
-    // }
+    //todo invader 24
 
-    // addInvader26();
+    //todo remove/add invaders
 
-    // //todo Move invaders 26
+    function removeInvader24(invadersMove) {
+        cells[invadersIndexBlock24].classList.remove("invader");
+    }
 
+    //removeInvader24();
 
 
+    function addInvader24() {
+        cells[invadersIndexBlock24].classList.add("invader");
 
-    // invadersStop = setInterval(moveInvaders26, 1000)
+    }
 
-    // //todo invader 28
+    //addInvader24();
 
-    // let invadersIndexBlock28 = 28
-    //     //todo remove/add invaders
+    //todo Move invaders 24
 
-    // function removeInvader28(invadersMove) {
-    //     cells[invadersIndexBlock28].classList.remove("invader");
-    // }
+    function moveInvaders24() {
 
-    // removeInvader28();
+        removeInvader24();
 
+        invadersIndexBlock24 += direction
 
-    // function addInvader28() {
-    //     cells[invadersIndexBlock28].classList.add("invader");
+        addInvader24()
 
-    // }
+        if (invadersIndexBlock24 > 89) {
+            scoretDisplay.innerHTML = "GAME OVER"
+            clearInterval(invadersStop24)
+        }
 
-    // addInvader28();
+    }
 
-    // //todo Move invaders
 
+    //todo invader 26
 
-    // invadersStop = setInterval(moveInvaders28, 1000)
 
+    //todo remove/add invaders
 
+    function removeInvader26(invadersMove) {
+        cells[invadersIndexBlock26].classList.remove("invader");
+    }
 
+    //removeInvader26();
+
+
+    function addInvader26() {
+        cells[invadersIndexBlock26].classList.add("invader");
+
+    }
+
+    //addInvader26();
+
+    //todo Move invaders 26
+
+    function moveInvaders26() {
+
+        removeInvader26();
+
+        invadersIndexBlock26 += direction
+
+        addInvader26()
+
+        if (invadersIndexBlock26 > 89) {
+            scoretDisplay.innerHTML = "GAME OVER"
+            clearInterval(invadersStop26)
+        }
+
+    }
+
+    //todo invader 28
+
+    //todo remove/add invaders
+
+    function removeInvader28(invadersMove) {
+        cells[invadersIndexBlock28].classList.remove("invader");
+    }
+
+    //removeInvader28();
+
+    function addInvader28() {
+        cells[invadersIndexBlock28].classList.add("invader");
+
+    }
+
+    //addInvader28();
+
+    //todo Move invaders 28
+
+    function moveInvaders28() {
+
+        removeInvader28();
+
+        invadersIndexBlock28 += direction
+
+        addInvader28();
+
+        if (invadersIndexBlock28 > 89) {
+            scoretDisplay.innerHTML = "GAME OVER"
+            clearInterval(invadersStop28)
+        }
+
+    }
     //todo laser shoot
 
     function shoot(e) {
         let laserId;
         let currentLaserIndex = currentSpaceshipIndex;
-
 
         function moveLaser() {
             cells[currentLaserIndex].classList.remove("laser")
@@ -500,11 +570,8 @@ function init() {
 
                     }
 
-                    // if (aliensIndicesRow1 < 1) {
-                    //     scoretDisplay.innerHTML = "You Win!"
-                    // }
+
             }
-            // cells[currentLaserIndex].classList.add("laser")
 
             if (currentLaserIndex < 10) {
                 cells[currentLaserIndex].classList.remove("laser")
@@ -525,6 +592,8 @@ function init() {
     console.log(start);
 
     resetButton.addEventListener('click', reset)
+
+
 
 
 }
